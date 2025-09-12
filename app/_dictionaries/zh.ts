89a206dd76375465ddf5d65b9aaec53d5f168653
king101125s@163.com <king101125s@gmail.com>
2025-09-12 18:16:44 +0800

    add：增加中文支持

---
+++ app/_dictionaries/zh.ts
@@ -0,0 +1,27 @@
+import type { Dictionary } from "./i18n-config";
+
+export default {
+  dark: "深色模式",
+  light: "浅色模式",
+  system: "系统默认",
+  backToTop: "回到顶部",
+  lastUpdated: "最后更新于",
+  logo: {
+    title: "React 数据获取 Hooks",
+  },
+  // notFound: '页面未找到',
+  poweredBy: "技术支持由",
+  link: {
+    vercel: "https://vercel.com/?utm_source=swr",
+  },
+  lightweight: "轻量级",
+  realtime: "实时更新",
+  suspense: "Suspense 支持",
+  pagination: "分页功能",
+  backendAgnostic: "后端无关",
+  renderingStrategies: "支持 SSR / SSG",
+  typescript: "TypeScript 支持",
+  remoteLocal: "远程 + 本地",
+  editPage: "在 GitHub 上编辑此页",
+  by: "作者",
+} satisfies Dictionary;
