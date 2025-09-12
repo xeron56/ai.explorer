32f38c3c31b9ff968f8e3f215a01f1fd99fc9f51
king101125s@163.com <king101125s@gmail.com>
2025-09-12 19:58:57 +0800

    Update ru.ts

--- app/_dictionaries/ru.ts
+++ app/_dictionaries/ru.ts
@@ -1,27 +1,31 @@
-import type { Dictionary } from './i18n-config'
+import type { Dictionary } from "./i18n-config";
 
 export default {
-  dark: 'Темный',
-  light: 'Светлый',
-  system: 'Системный',
-  backToTop: 'Перейти наверх',
-  lastUpdated: 'Последнее обновление',
+  dark: "Темный",
+  light: "Светлый",
+  system: "Системный",
+  backToTop: "Перейти наверх",
+  lastUpdated: "Последнее обновление",
   logo: {
-    title: 'React хуки для выборки данных'
+    title: "React хуки для выборки данных",
   },
   // notFound: 'Эта страница не может быть найдена',
-  poweredBy: 'Работает на',
+  poweredBy: "Работает на",
   link: {
-    vercel: 'https://vercel.com/?utm_source=swr_ru'
+    vercel: "https://vercel.com/?utm_source=swr_ru",
   },
-  lightweight: 'Лёгкий',
-  realtime: 'В реальном времени',
-  suspense: 'Задержка',
-  pagination: 'Пагинация',
-  backendAgnostic: 'Бэкэнд-независимый',
-  renderingStrategies: 'SSR / SSG',
-  typescript: 'TypeScript',
-  remoteLocal: 'Удалённо + Локально',
-  editPage: 'Редактировать на GitHub',
-  by: 'от'
-} satisfies Dictionary
+  lightweight: "Лёгкий",
+  realtime: "В реальном времени",
+  suspense: "Задержка",
+  pagination: "Пагинация",
+  backendAgnostic: "Бэкэнд-независимый",
+  renderingStrategies: "SSR / SSG",
+  typescript: "TypeScript",
+  remoteLocal: "Удалённо + Локально",
+  editPage: "Редактировать на GitHub",
+  by: "от",
+  banner: {
+    message: "Nextra Doc Template 🚀",
+    readMore: "Read more →",
+  },
+} satisfies Dictionary;
